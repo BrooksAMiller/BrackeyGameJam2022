4976918d2e5cc1eca83b5d53bd9505867ab5bc00
monorush <mark.mpe@gmail.com>
2022-02-25 22:58:55 +0100

    Small additions to level change

--- BrGameJam2022/Assets/Manhole.cs
+++ BrGameJam2022/Assets/Manhole.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement;
 public class Manhole : MonoBehaviour
 {
 
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
